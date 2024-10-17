243078024b707c30399d36ae159ffbdd5de6d5b9
Maisy1900 <mxr536@student.bham.ac.uk>
2024-10-17 15:49:04 +0100

    Animation movements

--- Assets/SimpleController.cs
+++ Assets/SimpleController.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using static UnityEngine.GraphicsBuffer;
+using System.IO.Enumeration;
+using System;
+using Unity.VisualScripting;
 
 public class SimpleController : MonoBehaviour
 {
     public ArticulationBody joint_1;
     public Transform joint_1_driver;
     public Rigidbody cube;
+    public string path;
+    public Animation rotateAnim;
+    private Animation playedAnim; 
+
+    public int[] targetAngle = new int[7];
+    private int counter = 0;
 
     private float startTime;
     private float finalTime; // to store the time when max velocity is reached
@@ -16,8 +27,25 @@ public class SimpleController : MonoBehaviour
     private bool reachedMaxVelocity = false;
     private float initialVelocity = 0f;
     private float mass = 1f; // mass of the object in kg
+    string allData; 
 
-    void FixedUpdate() // switching to FixedUpdate for more accurate physics
+    private void Start()
+    {
+        targetAngle = new int[] { 5, 10, 15, 20, 25, 30, 35 };
+
+    }
+
+    private void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            // Randomly/orderly select one of the animations and run them here.              
+            counter++;
+        }
+    }
+
+    void FixedUpdate()
     {
         Vector3 currentVelocity = cube.velocity;
 
@@ -46,6 +74,7 @@ public class SimpleController : MonoBehaviour
                 reachedMaxVelocity = true;
                 float timeToMaxVelocity = Time.time - startTime;
                 finalTime = Time.time; // capture the final time when max velocity is reached
+                          //Time.fixedTime;
 
                 // calculate acceleration using (v_f - v_i) / t
                 float finalVelocity = currentVelocity.magnitude; // assuming this is max velocity
@@ -63,7 +92,15 @@ public class SimpleController : MonoBehaviour
 
                 // print xDrive properties target and velocity
                 print("xDrive target " + jointDrive.target);
+                
+                allData = timeToMaxVelocity.ToString() + "," + finalVelocity.ToString() + "," + acceleration.ToString() + "," + force.ToString() + "," + jointDrive.target.ToString();
             }
+
+
+            int randNumb = UnityEngine.Random.Range(100, 999);
+            string fileName = "/" + randNumb.ToString() + "_physics_data.csv";
+            File.AppendAllText(Application.persistentDataPath + fileName, allData);
+            //File.WriteAllText(Application.persistentDataPath + fileName, allData);
 
             // update last velocity for next frame
             lastVelocity = currentVelocity.magnitude;
